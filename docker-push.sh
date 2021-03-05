946df89c3368dd89be0ee09690f3d7b18d4af0c0
benwilcock <bwilcock@vmware.com>
2021-03-05 11:23:58 +0000

    latest spring-native 0.9.0-snapshot fixes the config issues

--- docker-push.sh
+++ docker-push.sh
@@ -2,8 +2,10 @@
 
 docker push benwilcock/spring-petclinic-config-server:2.4.3
 docker push benwilcock/spring-petclinic-config-server-native:2.4.3
+docker push benwilcock/spring-petclinic-config-checker-native:2.4.3
 
 docker push benwilcock/spring-petclinic-discovery-server:2.4.3
+docker push benwilcock/spring-petclinic-discovery-checker:2.4.3
 
 docker push benwilcock/spring-petclinic-admin-server:2.4.3
 docker push benwilcock/spring-petclinic-admin-server-k8s:2.4.3
